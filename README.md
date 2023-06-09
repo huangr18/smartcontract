4b190f64799b27a242178e00717f18faf2727cda
huangr18 <huangr18@miamioh.edu>
2023-06-09 17:35:32 -0400

    changes name

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # smartcontract
+Change all "Asset" to "Donation"
+Change all "Owner" to "Donor"
--- smartcontract.go
+++ smartcontract.go
@@ -19,28 +19,28 @@ type Donation struct {
 	AppraisedValue int    `json:"AppraisedValue"`
 	Color          string `json:"Color"`
 	ID             string `json:"ID"`
-	Owner          string `json:"Owner"`
+	Donor          string `json:"Donor"`
 	Size           int    `json:"Size"`
 }
 
 // InitLedger adds a base set of assets to the ledger
 func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
-	assets := []Donation{
-		{ID: "asset1", Color: "blue", Size: 5, Owner: "Tomoko", AppraisedValue: 300},
-		{ID: "asset2", Color: "red", Size: 5, Owner: "Brad", AppraisedValue: 400},
-		{ID: "asset3", Color: "green", Size: 10, Owner: "Jin Soo", AppraisedValue: 500},
-		{ID: "asset4", Color: "yellow", Size: 10, Owner: "Max", AppraisedValue: 600},
-		{ID: "asset5", Color: "black", Size: 15, Owner: "Adriana", AppraisedValue: 700},
-		{ID: "asset6", Color: "white", Size: 15, Owner: "Michel", AppraisedValue: 800},
+	donations := []Donation{
+		{ID: "asset1", Color: "blue", Size: 5, Donor: "Tomoko", AppraisedValue: 300},
+		{ID: "asset2", Color: "red", Size: 5, Donor: "Brad", AppraisedValue: 400},
+		{ID: "asset3", Color: "green", Size: 10, Donor: "Jin Soo", AppraisedValue: 500},
+		{ID: "asset4", Color: "yellow", Size: 10, Donor: "Max", AppraisedValue: 600},
+		{ID: "asset5", Color: "black", Size: 15, Donor: "Adriana", AppraisedValue: 700},
+		{ID: "asset6", Color: "white", Size: 15, Donor: "Michel", AppraisedValue: 800},
 	}
 
-	for _, asset := range assets {
-		assetJSON, err := json.Marshal(asset)
+	for _, donation := range donations {
+		donationJSON, err := json.Marshal(donation)
 		if err != nil {
 			return err
 		}
 
-		err = ctx.GetStub().PutState(asset.ID, assetJSON)
+		err = ctx.GetStub().PutState(donation.ID, donationJSON)
 		if err != nil {
 			return fmt.Errorf("failed to put to world state. %v", err)
 		}
@@ -50,42 +50,42 @@ func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) 
 }
 
 // CreateAsset issues a new asset to the world state with given details.
-func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, color string, size int, owner string, appraisedValue int) error {
-	exists, err := s.AssetExists(ctx, id)
+func (s *SmartContract) CreateDonation(ctx contractapi.TransactionContextInterface, id string, color string, size int, donor string, appraisedValue int) error {
+	exists, err := s.DonationExists(ctx, id)
 	if err != nil {
 		return err
 	}
 	if exists {
-		return fmt.Errorf("the asset %s already exists", id)
+		return fmt.Errorf("the donation %s already exists", id)
 	}
 
-	asset := Donation{
+	donation := Donation{
 		ID:             id,
 		Color:          color,
 		Size:           size,
-		Owner:          owner,
+		Donor:          donor,
 		AppraisedValue: appraisedValue,
 	}
-	assetJSON, err := json.Marshal(asset)
+	donationJSON, err := json.Marshal(donation)
 	if err != nil {
 		return err
 	}
 
-	return ctx.GetStub().PutState(id, assetJSON)
+	return ctx.GetStub().PutState(id, donationJSON)
 }
 
 // ReadAsset returns the asset stored in the world state with given id.
-func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, id string) (*Asset, error) {
-	assetJSON, err := ctx.GetStub().GetState(id)
+func (s *SmartContract) ReadDonation(ctx contractapi.TransactionContextInterface, id string) (*Donation, error) {
+	donationJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read from world state: %v", err)
 	}
-	if assetJSON == nil {
+	if donationJSON == nil {
 		return nil, fmt.Errorf("the asset %s does not exist", id)
 	}
 
 	var asset Donation
-	err = json.Unmarshal(assetJSON, &asset)
+	err = json.Unmarshal(donationJSON, &asset)
 	if err != nil {
 		return nil, err
 	}
@@ -94,79 +94,79 @@ func (s *SmartContract) ReadAsset(ctx contractapi.TransactionContextInterface, i
 }
 
 // UpdateAsset updates an existing asset in the world state with provided parameters.
-func (s *SmartContract) UpdateAsset(ctx contractapi.TransactionContextInterface, id string, color string, size int, owner string, appraisedValue int) error {
-	exists, err := s.AssetExists(ctx, id)
+func (s *SmartContract) UpdateDonation(ctx contractapi.TransactionContextInterface, id string, color string, size int, donor string, appraisedValue int) error {
+	exists, err := s.DonationExists(ctx, id)
 	if err != nil {
 		return err
 	}
 	if !exists {
-		return fmt.Errorf("the asset %s does not exist", id)
+		return fmt.Errorf("the donation %s does not exist", id)
 	}
 
 	// overwriting original asset with new asset
-	asset := Donation{
+	donation := Donation{
 		ID:             id,
 		Color:          color,
 		Size:           size,
-		Owner:          owner,
+		Donor:          donor,
 		AppraisedValue: appraisedValue,
 	}
-	assetJSON, err := json.Marshal(asset)
+	donationJSON, err := json.Marshal(donation)
 	if err != nil {
 		return err
 	}
 
-	return ctx.GetStub().PutState(id, assetJSON)
+	return ctx.GetStub().PutState(id, donationJSON)
 }
 
 // DeleteAsset deletes an given asset from the world state.
-func (s *SmartContract) DeleteAsset(ctx contractapi.TransactionContextInterface, id string) error {
-	exists, err := s.AssetExists(ctx, id)
+func (s *SmartContract) DeleteDonation(ctx contractapi.TransactionContextInterface, id string) error {
+	exists, err := s.DonationExists(ctx, id)
 	if err != nil {
 		return err
 	}
 	if !exists {
-		return fmt.Errorf("the asset %s does not exist", id)
+		return fmt.Errorf("the donation %s does not exist", id)
 	}
 
 	return ctx.GetStub().DelState(id)
 }
 
 // AssetExists returns true when asset with given ID exists in world state
-func (s *SmartContract) AssetExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
-	assetJSON, err := ctx.GetStub().GetState(id)
+func (s *SmartContract) DonationExists(ctx contractapi.TransactionContextInterface, id string) (bool, error) {
+	donationJSON, err := ctx.GetStub().GetState(id)
 	if err != nil {
 		return false, fmt.Errorf("failed to read from world state: %v", err)
 	}
 
-	return assetJSON != nil, nil
+	return donationJSON != nil, nil
 }
 
 // TransferAsset updates the owner field of asset with given id in world state, and returns the old owner.
-func (s *SmartContract) TransferAsset(ctx contractapi.TransactionContextInterface, id string, newOwner string) (string, error) {
-	asset, err := s.ReadAsset(ctx, id)
+func (s *SmartContract) TransferAsset(ctx contractapi.TransactionContextInterface, id string, newDonor string) (string, error) {
+	donation, err := s.ReadDonation(ctx, id)
 	if err != nil {
 		return "", err
 	}
 
-	oldOwner := asset.Owner
-	asset.Owner = newOwner
+	oldDonor := donation.Donor
+	donation.Donor = newDonor
 
-	assetJSON, err := json.Marshal(asset)
+	donationJSON, err := json.Marshal(donation)
 	if err != nil {
 		return "", err
 	}
 
-	err = ctx.GetStub().PutState(id, assetJSON)
+	err = ctx.GetStub().PutState(id, donationJSON)
 	if err != nil {
 		return "", err
 	}
 
-	return oldOwner, nil
+	return oldDonor, nil
 }
 
 // GetAllAssets returns all assets found in world state
-func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface) ([]*Asset, error) {
+func (s *SmartContract) GetAllDonations(ctx contractapi.TransactionContextInterface) ([]*Donation, error) {
 	// range query with empty string for startKey and endKey does an
 	// open-ended query of all assets in the chaincode namespace.
 	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
@@ -175,7 +175,7 @@ func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface
 	}
 	defer resultsIterator.Close()
 
-	var assets []*Donation
+	var donations []*Donation
 	for resultsIterator.HasNext() {
 		queryResponse, err := resultsIterator.Next()
 		if err != nil {
@@ -187,8 +187,8 @@ func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface
 		if err != nil {
 			return nil, err
 		}
-		assets = append(assets, &asset)
+		donations = append(donations, &asset)
 	}
 
-	return assets, nil
+	return donations, nil
 }
